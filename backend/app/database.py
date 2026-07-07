4b977950772616159907903135b8bbd962f00295
Osvaldo Perez Angel <7valdo10@gmail.com>
2026-07-07 17:42:23 -0600

    Fix production database config

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 from pathlib import Path
+import os
 
 # DATABASE_PATH = Path(__file__).resolve().parent.parent / "creditsim.db"
 # DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
@@ -10,10 +11,16 @@ DATABASE_URL = os.getenv(
     "DATABASE_URL",
     f"sqlite:///{DATABASE_PATH}"
 )
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+
+    connect_args = {
+        "check_same_thread": False
+    }
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(
